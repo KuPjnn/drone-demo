009986bc54815d862f7b1456fa3f634f2f083f3d
kupin <l0vemarrykun@gmail.com>
2024-03-03 00:35:06 +0700

    initial code

---
+++ src/main/java/com/drone/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.drone.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    public List<String> getListString() {
+        return Arrays.asList("Demo", "Long");
+    }
+
+}
